6c7d42a694d57a069c42bdbb9a590a3f047050c8
sangjo_itwill <sangjo_itwill@local.local>
2013-12-10 10:19:19 +0900

    Initial Commit

---
+++ cocos2d_study/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  cocos2d_study
+//
+//  Created by sangjo_itwill on 2013. 12. 10..
+//  Copyright sj 2013년. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
